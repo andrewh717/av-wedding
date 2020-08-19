330b97015babb0c85496b82bf2159dd48faf01f5
Andrew H <andrewh717@gmail.com>
2020-08-19 17:10:13 -0400

    Added Firestore

--- public/script.js
+++ public/script.js
@@ -1,9 +1,14 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzYM3D-VZDfpc7Y2W0mb-aGJzXcA5MErQ63NCE3ROjhTKi8_-6i/exec'
-  const form = document.forms['rsvp-form']
+console.log(firebase);
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzYM3D-VZDfpc7Y2W0mb-aGJzXcA5MErQ63NCE3ROjhTKi8_-6i/exec'
+const form = document.forms['rsvp-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, {
+      method: 'POST',
+      body: new FormData(form)
+    })
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
