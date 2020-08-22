6a43052344c7a87894a99568187dc9dd15b0c80b
Andrew H <andrewh717@gmail.com>
2020-08-21 22:08:45 -0400

    Migrating to React to create RSVP form

--- public/script.js
+++ public/script.js
@@ -1,14 +1,33 @@
+'use strict';
+import RsvpModal from './rsvp-modal';
+ReactDOM.render(<RsvpModal />, document.querySelector('rsvp-modal-container'));
+
 console.log(firebase);
+const db = firebase.firestore();
+
+let guestsRef;
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzYM3D-VZDfpc7Y2W0mb-aGJzXcA5MErQ63NCE3ROjhTKi8_-6i/exec'
 const form = document.forms['rsvp-form']
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, {
-      method: 'POST',
-      body: new FormData(form)
-    })
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
+  e.preventDefault();
+  let formData = new FormData(form);
+  let rsvp = JSON.parse(JSON.stringify(Object.fromEntries(formData)));
+  console.log(rsvp);
+  guestsRef = db.collection('guests');
+  const { serverTimestamp } = firebase.firestore.FieldValue;
+  guestsRef.add({
+    name: rsvp.name,
+    attending: rsvp.response,
+    timestamp: serverTimestamp()
+  });
+
+  // fetch(scriptURL, {
+  //     method: 'POST',
+  //     body: new FormData(form)
+  //   })
+  //   .then(response => console.log('Success!', response))
+  //   .catch(error => console.error('Error!', error.message))
+  // $('#rsvpModal').modal('hide');
 })
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import RsvpModal from './components/RsvpModal';
+
+function App() {
+  return(
+    <div>
+      <RsvpModal />
+    </div>
+  );
+}
+
+export default App;
