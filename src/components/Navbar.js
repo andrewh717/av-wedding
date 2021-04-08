c9085531a9b07e809b5908309e503de1787a7c45
Andrew H <andrewh717@gmail.com>
2021-04-08 19:19:56 -0400

    Working on fixing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,57 +6,60 @@ const Navbar = () => {
   return (
     <div>
       <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link
-          to={(location) => {
-            return route(location.pathname, '/');
-          }}
-        >
-          A&amp;V
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <HashLink smooth to="/#story">
-              Our Story <span className="sr-only">(current)</span>
-              </HashLink>
-            </li>
-            <li className="nav-item active">
-              <HashLink smooth to="/#travel">
-                Travel <span className="sr-only">(current)</span>
-              </HashLink>
-            </li>
-            <li className="nav-item active">
-              <HashLink smooth to="/#weddingparty">
-                Wedding Party <span className="sr-only">(current)</span>
-              </HashLink>
-            </li>
-            <li className="nav-item active">
-              <HashLink smooth to="/#photos">
-                Photos <span className="sr-only">(current)</span>
-              </HashLink>
-            </li>
-            <li className="nav-item active">
-              <HashLink smooth to="/#registry">
-                Registry <span className="sr-only">(current)</span>
-              </HashLink>
-            </li>
-            <li className="nav-item active">
-              <HashLink smooth to="/#faq">
-                FAQ <span className="sr-only">(current)</span>
-              </HashLink>
-            </li>
-          </ul>
+        <div className="container-fluid">
+          <Link
+            className="navbar-brand"
+            to={(location) => {
+              return route(location.pathname, '/');
+            }}
+          >
+            A&amp;V
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <HashLink smooth to="/#story">
+                  Our Story <span className="sr-only">(current)</span>
+                </HashLink>
+              </li>
+              <li className="nav-item active">
+                <HashLink smooth to="/#travel">
+                  Travel <span className="sr-only">(current)</span>
+                </HashLink>
+              </li>
+              <li className="nav-item active">
+                <HashLink smooth to="/#weddingparty">
+                  Wedding Party <span className="sr-only">(current)</span>
+                </HashLink>
+              </li>
+              <li className="nav-item active">
+                <HashLink smooth to="/#photos">
+                  Photos <span className="sr-only">(current)</span>
+                </HashLink>
+              </li>
+              <li className="nav-item active">
+                <HashLink smooth to="/#registry">
+                  Registry <span className="sr-only">(current)</span>
+                </HashLink>
+              </li>
+              <li className="nav-item active">
+                <HashLink smooth to="/#faq">
+                  FAQ <span className="sr-only">(current)</span>
+                </HashLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
