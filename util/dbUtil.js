e31e17c32d232a34c5bc8e7a47bd023e8346c738
Andrew Hernandez <andrewh717@gmail.com>
2022-03-02 10:35:13 -0500

    Added ~util for getting hasNotResponded list

--- util/dbUtil.js
+++ util/dbUtil.js
@@ -1,13 +1,16 @@
 const admin = require('firebase-admin');
+const csv = require('@fast-csv/format');
+const fs = require('fs');
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
   databaseURL: 'https://av-wedding-2022.firebaseio.com',
 });
 
+const db = admin.firestore();
+const batch = db.batch();
+
 const updateAllDocs = () => {
-  const db = admin.firestore();
-  const batch = db.batch();
   db.collection('guests')
     .get()
     .then((snapshot) => {
@@ -25,6 +28,26 @@ const updateAllDocs = () => {
     });
 }
 
-updateAllDocs();
+const getHasNotRespondedList = () => {
+  const writeStream = fs.createWriteStream('hasNotResponded.csv', { flags: 'a' });
+  const hasNotRespondedList = [];
+  db.collection('guests')
+      .where('hasResponded', '==', false)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((guest) => {
+          hasNotRespondedList.push(guest.data());
+        });
+        csv.write(hasNotRespondedList, { headers: true }).pipe(writeStream);
+      })
+      .catch((error) => {
+        console.log('Error getting documents: ', error);
+      });
+}
+
+
+
+// updateAllDocs();
+// getHasNotRespondedList();
 
 // GOOGLE_APPLICATION_CREDENTIALS="./serviceAccountKey.json" node dbUtil.js
